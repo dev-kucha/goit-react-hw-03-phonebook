03f90c916160da679548a9f44cca3787392ccb1b
Yurii <ukucher@gmail.com>
2022-08-13 00:25:43 +0300

    add Formik, yup and yup-phone

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,19 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import styled from 'styled-components';
+import * as yup from 'yup';
+import 'yup-phone';
+
+const Input = styled(Field)`
+  color: #2a2a2a;
+  font-size: 16px;
+`;
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  number: yup.string().phone('UA').required(),
+});
 
 export class App extends Component {
   state = {
@@ -17,11 +31,8 @@ export class App extends Component {
       return { contacts: [contact, ...prevState.contacts] };
     });
   };
-  handleSubmit = ({
-    name: { value: incomName },
-    number: { value: incomNumber },
-  }) => {
-    this.addContact(this.createContact(incomName, incomNumber));
+  handleSubmit = ({ name, number }) => {
+    this.addContact(this.createContact(name, number));
   };
 
   render() {
@@ -38,41 +49,62 @@ export class App extends Component {
       >
         <h2>Phonebook</h2>
         <h3>Name</h3>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
+        <Formik
+          initialValues={{ name: '', number: '' }}
+          validationSchema={schema}
+          onSubmit={(values, { resetForm }) => {
+            console.log(values);
+            this.handleSubmit(values);
+            // console.log(actions);
+            resetForm();
+            // e.preventDefault();
             // console.log(e.target.elements.name.value);
-            // const { name } = e.target.elements;
-            // console.log(name.value);
+            // const { name } = target.elements;
             // this.addContact({ name: name, id: nanoid() });
-            this.handleSubmit(e.target.elements);
-            e.target.reset();
+            // this.handleSubmit(e.target.elements);
+            // e.target.reset();
           }}
         >
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <label>
-            Number
-            <input
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />
-          </label>
-          <button type="submit" name="addContact">
-            Add contact
-          </button>
-        </form>
+          <Form
+          // onSubmit={e => {
+          //   e.preventDefault();
+          //   // console.log(e.target.elements.name.value);
+          //   // const { name } = e.target.elements;
+          //   // console.log(name.value);
+          //   // this.addContact({ name: name, id: nanoid() });
+          //   this.handleSubmit(e.target.elements);
+          //   e.target.reset();
+          // }}
+          >
+            <label>
+              Name
+              <Input
+                type="text"
+                name="name"
+                // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                // required
+              />
+              <ErrorMessage name="name" component="div" />
+            </label>
+            <br />
+            <label>
+              Number
+              <Input
+                type="tel"
+                name="number"
+                // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                // required
+              />
+              <ErrorMessage name="number" component="div" />
+            </label>
+            <br />
+            <button type="submit" name="addContact">
+              Add contact
+            </button>
+          </Form>
+        </Formik>
         <h2>Contacts</h2>
         <ul>
           {this.state.contacts.map((contact, idx) => {
